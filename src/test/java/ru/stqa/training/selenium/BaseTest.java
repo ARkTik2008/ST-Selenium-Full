6f5c3f31c5ebeae6206388dcd970844351930b34
Nikolay Malov <n.malov92@gmail.com>
2018-05-16 21:23:50 +0300

    Обновлен BaseTest
    Обновлен BaseTest

--- src/test/java/ru/stqa/training/selenium/BaseTest.java
+++ src/test/java/ru/stqa/training/selenium/BaseTest.java
@@ -10,6 +10,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.support.events.AbstractWebDriverEventListener;
+import org.openqa.selenium.support.events.EventFiringWebDriver;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -19,12 +21,29 @@ import java.util.concurrent.TimeUnit;
 
 public class BaseTest {
 
-    public WebDriver driver;
+    public EventFiringWebDriver driver;
     public WebDriverWait wait;
+    public static class MyListener extends AbstractWebDriverEventListener{
+       /* @Override
+        public void beforeFindBy(By by, WebElement element, WebDriver driver) {
+            System.out.println(by);
+        }
+
+        @Override
+        public void onException(Throwable throwable, WebDriver driver) {
+            System.out.println(throwable);
+        }
+
+        @Override
+        public void afterFindBy(By by, WebElement element, WebDriver driver) {
+            System.out.println(by + "found");
+        }*/
+    }
 
     @Before
     public void start() {
-        driver = new ChromeDriver();
+        driver = new EventFiringWebDriver(new ChromeDriver());
+        driver.register(new MyListener());
         driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
         wait = new WebDriverWait(driver, 5);
     }
